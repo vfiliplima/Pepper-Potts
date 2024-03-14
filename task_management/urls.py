215fe5aa6dd18278ed3c04c0fb7b3b3adaf1a8ee
Amyris <vlima@amyris.com>
2024-03-14 22:09:12 +0000

    remove index view

--- task_management/urls.py
+++ task_management/urls.py
@@ -22,7 +22,6 @@ from tasks.views import (
     TaskRetrieveUpdateDestroyView,
     task_list,
     task_detail,
-    index,
     task_delete,
     create_task,
 )
@@ -35,7 +34,6 @@ urlpatterns = [
         TaskRetrieveUpdateDestroyView.as_view(),
         name="task-update-destroy",
     ),
-    path("", index, name="home-page"),
     path("task-list/", task_list, name="view-tasks"),
     path("task/<int:pk>/detail/", task_detail, name="task-detail-page"),
     path("task/<int:pk>/delete/", task_delete, name="task-delete"),
