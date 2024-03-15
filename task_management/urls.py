576433d3994982e705d56e11e4ef30bb1bec9a7f
Amyris <vlima@amyris.com>
2024-03-15 14:23:51 +0000

    authintication and login

--- task_management/urls.py
+++ task_management/urls.py
@@ -17,25 +17,36 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
 from tasks.views import (
     TaskListCreateView,
     TaskRetrieveUpdateDestroyView,
+    create_task,
     task_list,
     task_detail,
+    task_update,
     task_delete,
-    create_task,
 )
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # path("accounts/", include("django.contrib.auth.urls")),
     path("tasks/", TaskListCreateView.as_view(), name="task-list-create"),
     path(
         "tasks/<int:pk>/",
         TaskRetrieveUpdateDestroyView.as_view(),
         name="task-update-destroy",
     ),
+    path(
+        "accounts/login/",
+        LoginView.as_view(template_name="home.html", next_page="view-tasks"),
+        name="login",
+    ),
+    path("logout/", LogoutView.as_view(next_page="view-tasks"), name="logout"),
+    path("task/create/", create_task, name="task-create"),
     path("task-list/", task_list, name="view-tasks"),
     path("task/<int:pk>/detail/", task_detail, name="task-detail-page"),
+    path("task/<int:pk>/update/", task_update, name="task-update"),
     path("task/<int:pk>/delete/", task_delete, name="task-delete"),
-    path("task/create/", create_task, name="task-create"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -4,6 +4,7 @@ from .models import Task
 from .serializers import TaskSerializer
 from .forms import TaskForm
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 
 
 # from django.urls import reverse
@@ -21,8 +22,19 @@ class TaskRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
 
 
 def task_list(request):
+    priority = request.GET.get(
+        "priority", None
+    )  # Get the priority from the request query parameters
     tasks = Task.objects.all()
-    return render(request, "task_list.html", {"tasks": tasks})
+
+    # Filter tasks based on priority if a priority is selected
+    if priority:
+        tasks = tasks.filter(priority=priority)
+
+    context = {
+        "tasks": tasks,
+    }
+    return render(request, "task_list.html", context)
 
 
 @login_required
@@ -30,12 +42,24 @@ def task_detail(request, pk):
     # Retrieve the task object from the database using its ID
     task = get_object_or_404(Task, pk=pk)
 
+    # Check if the logged-in user matches the user assigned to the task
+    if task.user != request.user:
+        # If the logged-in user is not the assigned user, return a 403 Forbidden response
+        return HttpResponseForbidden("You don't have permission to view this task.")
+
     # Pass the task object to the template for rendering
     return render(request, "task_detail.html", {"task": task})
 
 
 @login_required
 def task_delete(request, pk):
+    print("")
+    print("---")
+    print(request.method)
+    token = request.user
+    print(token)
+    print("---")
+    print("")
     # Retrieve the task object from the database using its ID
     task = get_object_or_404(Task, pk=pk)
 
@@ -43,8 +67,8 @@ def task_delete(request, pk):
     if request.method == "POST":
         # Delete the task object
         task.delete()
-        # Redirect to a success URL (e.g., task list page)
-        return redirect("task-list")
+        # Redirect to the task list page
+        return redirect("view-tasks")
     else:
         # If the request method is not POST, render a confirmation page
         return render(request, "task_delete_confirm.html", {"task": task})
@@ -61,3 +85,28 @@ def create_task(request):
     else:
         form = TaskForm()
     return render(request, "create_task.html", {"form": form})
+
+
+def task_update(request, pk):
+    # Retrieve the task object from the database using its ID
+    task = get_object_or_404(Task, pk=pk)
+
+    print("")
+    print("")
+    print(request.method)
+    print("")
+    print("")
+
+    if request.method == "PUT":
+        # Process the form submission data
+        task.title = request.POST.get("editTitle")
+        task.description = request.POST.get("editDescription")
+        task.due_date = request.POST.get("editDueDate")
+        task.priority = request.POST.get("editPriority")
+        task.save()
+
+        # Redirect to the task detail page or any other appropriate page
+        return redirect("task-detail-page", pk=pk)
+
+    # If the request method is not POST, render the same page with the form
+    return render(request, "task_detail.html", {"task": task})
